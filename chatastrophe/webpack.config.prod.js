6103ed2de529586d100d8fbbad8ce80485d71464
Michael Luo <mluo@seven.com.au>
2018-12-10 10:22:46 +1100

    update prod webpack build

--- chatastrophe/webpack.config.prod.js
+++ chatastrophe/webpack.config.prod.js
@@ -1,47 +1,70 @@
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ManifestPlugin = require('webpack-manifest-plugin');
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var ManifestPlugin = require("webpack-manifest-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
-  entry: __dirname + '/src/index.js',
+  entry: __dirname + "/src/index.js",
   output: {
-    path: __dirname + '/build',
-    filename: 'bundle.js',
-    publicPath: './'
+    path: __dirname + "/build",
+    filename: "bundle.js",
+    publicPath: "./"
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react'],
-          plugins: ['react-hot-loader/babel', 'transform-class-properties']
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ["@babel/plugin-proposal-class-properties"]
+          }
         }
       },
       {
         test: /\.css$/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
       },
       {
         exclude: [/\.html$/, /\.(js|jsx)$/, /\.css$/, /\.json$/],
-        loader: 'file-loader',
-        options: {
-          name: 'static/media/[name].[ext]'
-        }
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "static/media/[name].[ext]"
+            }
+          }
+        ]
       }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            warnings: false,
+            reduce_vars: false
+          },
+          output: {
+            comments: false
+          }
+        },
+        sourceMap: true
+      })
     ]
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
       }
     }),
     new HtmlWebpackPlugin({
       inject: true,
-      template: __dirname + '/public/index.html',
+      template: __dirname + "/public/index.html",
       minify: {
         removeComments: true,
         collapseWhitespace: true,
@@ -55,18 +78,9 @@ module.exports = {
         minifyURLs: true
       }
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        reduce_vars: false
-      },
-      output: {
-        comments: false
-      },
-      sourceMap: true
-    }),
+
     new ManifestPlugin({
-      fileName: 'asset-manifest.json'
+      fileName: "asset-manifest.json"
     })
   ]
 };
